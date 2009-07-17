0c3b9e571c419dd896180bcb0e773d8ebb1a0bef
Marko Krause <zeratul2099@googlemail.com>
2009-07-17 20:30:42 +0200

    more background

--- media/svg2png.py
+++ media/svg2png.py
@@ -2,8 +2,8 @@
 
 import os
 
-for i in range(100):
-    os.system("rasterizer -d png/absorb%d.png -snapshotTime %f absorb.svg"%(i, float(i)/4))
-    os.chdir("png")
-    os.system("convert -dispose previous -delay 10 -size 100x100 -geometry 800x800 -page +0+0 *.png animation.gif")
+#for i in range(88):
+#    os.system("rasterizer -d png/squeeze%2.2d.png -snapshotTime %f squeeze.svg"%(i, float(i)/4))
+os.chdir("png")
+os.system("convert -dispose previous -delay 25 -geometry 600x600 -page +0+0 *.png animation.gif")
 
