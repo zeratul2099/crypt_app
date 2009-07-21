b3c27e9bcb2719231a3192b024aaf13ecf914e07
Marko Krause <zeratul2099@googlemail.com>
2009-07-21 16:49:31 +0200

    chi.gif and pi.svg

--- media/svg2png.py
+++ media/svg2png.py
@@ -4,8 +4,9 @@ import os
 
 name = "chi"
 
-for i in range(360):
-    os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg"%(name, i, float(i)/4, name))
+for i in range(80, 90, 2):
+    os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg &"%(name, i, float(i)/1, name))
+    os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg"%(name, i+1, float(i+1)/1, name))
 os.chdir("png")
 os.system("convert -dispose previous -delay 25 -page +0+0 %s*.png %s.gif"%(name, name))
 
