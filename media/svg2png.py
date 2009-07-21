0d380bb2f3349b64e9de1abd4461e8fb54ab886c
Marko Krause <zeratul2099@googlemail.com>
2009-07-21 04:35:22 +0200

    preparation for chi.gif

--- media/svg2png.py
+++ media/svg2png.py
@@ -2,8 +2,10 @@
 
 import os
 
-for i in range(100):
-    os.system("rasterizer -d png/absorb%2.2d.png -snapshotTime %f absorb.svg"%(i, float(i)/4))
+name = "chi"
+
+for i in range(360):
+    os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg"%(name, i, float(i)/4, name))
 os.chdir("png")
-os.system("convert -dispose previous -delay 25 -page +0+0 absorb*.png animation.gif")
+os.system("convert -dispose previous -delay 25 -page +0+0 %s*.png %s.gif"%(name, name))
 
