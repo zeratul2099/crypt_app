07f2c16805eaffce035792e2b4987abc1cd535b7
Marko Krause <zeratul2099@googlemail.com>
2009-09-29 20:54:32 +0200

    md5.gif

---
+++ media/gif/svg2png.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+
+name = "sha1"
+
+for i in range(0, 180, 2):
+    os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg &"%(name, i, float(i)/4, name))
+    os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg"%(name, i+1, float(i+1)/4, name))
+os.chdir("png")
+os.system("convert -dispose previous -delay 25 -page +0+0 %s*.png %s.gif"%(name, name))
+
