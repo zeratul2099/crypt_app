bd4d84fffd5019e256f75790cf5c9b3c320d46dc
Marko Krause <zeratul2099@googlemail.com>
2009-10-02 00:22:47 +0200

    multiprocessing

--- stego/views.py
+++ stego/views.py
@@ -62,47 +62,92 @@ def algo(request, algo):
                 embedForm = LsbEmbedForm(request.POST, request.FILES)
                 extractForm = LsbExtractForm()
                 if embedForm.is_valid():
-                    retval = lsbEmbed(request)
+                    p = Process(target=lsbEmbed, args=(q, ))
+                    p.start()
+                    q.put([request.POST, request.FILES['file'].temporary_file_path()])
+                    try:
+                        retval = q.get(True, 10)
+                    except Q.Empty:
+                        retval = -2
+                    p.join()
                     if retval == -1:
                         text += "\nNo PNG File or PNG file could not be read."
+                    elif retval == -2:
+                        text += "Error on embedding. Try another image or different parameter"
                     else:
-                        return retval
+                        return createResponse(retval, "png")
             elif algo == "gifshuffle":
                 embedForm = GifShuffleEmbedForm(request.POST, request.FILES)
                 extractForm = GifShuffleExtractForm()
                 if embedForm.is_valid():
-                    retval = gifShuffleEmbed(request)
+                    p = Process(target=gifShuffleEmbed, args=(q, ))
+                    p.start()
+                    q.put([request.POST, request.FILES['file'].temporary_file_path()])
+                    try:
+                        retval = q.get(True, 10)
+                    except Q.Empty:
+                        retval = -2
+                    p.join()
                     if retval == -1:
                         text += "\nNo GIF File or GIF file could not be read."
+                    elif retval == -2:
+                        text += "Error on embedding. Try another image or different parameter"
                     else:
-                        return retval
+                        return createResponse(retval, "gif")
             elif algo == "bs":
                 embedForm = BattlestegEmbedForm(request.POST, request.FILES)
                 extractForm = BattlestegExtractForm()
                 if embedForm.is_valid():
-                    retval = bsEmbed(request)
+                    p = Process(target=bsEmbed, args=(q, ))
+                    p.start()
+                    q.put([request.POST, request.FILES['file'].temporary_file_path()])
+                    try:
+                        retval = q.get(True, 10)
+                    except Q.Empty:
+                        retval = -2
+                    p.join()
                     if retval == -1:
                         text += "\nNo PNG File or PNG file could not be read."
+                    elif retval == -2:
+                        text += "Error on embedding. Try another image or different parameter"
                     else:
-                        return retval
+                        return createResponse(retval, "png")
         elif "submit2" in request.POST:
     
             if algo == "cpt":
                 embedForm = CPTEmbedForm()
                 extractForm = CPTExtractForm(request.POST, request.FILES)
                 if extractForm.is_valid():
-                    retval = cptExtract(request)
+                    p = Process(target=cptExtract, args=(q, ))
+                    p.start()
+                    q.put([request.POST, request.FILES['file'].temporary_file_path()])
+                    try:
+                        retval = q.get(True, 10)
+                    except Q.Empty:
+                        retval = -2
+                    p.join()
                     if retval == -1:
                         text += "\nNo PNG File or PNG file could not be read."
+                    elif retval == -2:
+                        text += "Error on embedding. Try another image or different parameter"
                     else:
                         text += retval
             elif algo == "f5":
                 embedForm = F5EmbedForm()
                 extractForm = F5ExtractForm(request.POST, request.FILES)
                 if extractForm.is_valid():
-                    retval = f5Extract(request)
+                    p = Process(target=f5Extract, args=(q, ))
+                    p.start()
+                    q.put([request.POST, request.FILES['file'].temporary_file_path()])
+                    try:
+                        retval = q.get(True, 10)
+                    except Q.Empty:
+                        retval = -2
+                    p.join()
                     if retval == -1:
                         text += "\nNo JPEG File or JPEG file could not be read."
+                    elif retval == -2:
+                        text += "Error on embedding. Try another image or different parameter"
                     else:
                         text += retval
 
@@ -110,9 +155,18 @@ def algo(request, algo):
                 embedForm = LsbEmbedForm()
                 extractForm = LsbExtractForm(request.POST, request.FILES)
                 if extractForm.is_valid():
-                    retval = lsbExtract(request)
+                    p = Process(target=lsbExtract, args=(q, ))
+                    p.start()
+                    q.put([request.POST, request.FILES['file'].temporary_file_path()])
+                    try:
+                        retval = q.get(True, 10)
+                    except Q.Empty:
+                        retval = -2
+                    p.join()
                     if retval == -1:
                         text += "\nNo PNG File or PNG file could not be read."
+                    elif retval == -2:
+                        text += "Error on embedding. Try another image or different parameter"
                     else:
                         text += retval
             elif algo == "gifshuffle":
@@ -120,19 +174,36 @@ def algo(request, algo):
                 embedForm = GifShuffleEmbedForm()
                 extractForm = GifShuffleExtractForm(request.POST, request.FILES)
                 if extractForm.is_valid():
-                    print "form valid"
-                    retval = gifShuffleExtract(request)
+                    p = Process(target=gifShuffleExtract, args=(q, ))
+                    p.start()
+                    q.put([request.POST, request.FILES['file'].temporary_file_path()])
+                    try:
+                        retval = q.get(True, 10)
+                    except Q.Empty:
+                        retval = -2
+                    p.join()
                     if retval == -1:
                         text += "\nNo GIF File or GIF file could not be read."
+                    elif retval == -2:
+                        text += "Error on embedding. Try another image or different parameter"
                     else:
                         text += retval
             elif algo == "bs":
                 embedForm = BattlestegEmbedForm()
                 extractForm = BattlestegExtractForm(request.POST, request.FILES)
                 if extractForm.is_valid():
-                    retval = bsExtract(request)
+                    p = Process(target=bsExtract, args=(q, ))
+                    p.start()
+                    q.put([request.POST, request.FILES['file'].temporary_file_path()])
+                    try:
+                        retval = q.get(True, 10)
+                    except Q.Empty:
+                        retval = -2
+                    p.join()
                     if retval == -1:
                         text += "\nNo PNG File or PNG file could not be read."
+                    elif retval == -2:
+                        text += "Error on embedding. Try another image or different parameter"
                     else:
                         text += retval
                 
@@ -195,23 +266,24 @@ def cptEmbed(q):
 
 
 
-def cptExtract(request):
+def cptExtract(q):
+    post, filename = q.get()
     rgb_data = libstegofile.rgb_data_t()
     png_struct = libstegofile.png_internal_data_t()
-    retcode = libstegofile.io_png_read(request.FILES['file'].temporary_file_path(),rgb_data, png_struct)
-    if retcode == 0 and request.FILES['file'].content_type == "image/png":
+    retcode = libstegofile.io_png_read(filename, rgb_data, png_struct)
+    if retcode == 0:
         pass
     else:
         return -1
     para = libstegofile.cpt_parameter()
-    para.password = str(request.POST['pw'])
+    para.password = str(post['pw'])
     para.pwlen = len(para.password)
-    para.block_width = int((request.POST['width']))
-    para.block_height = int((request.POST['height']))
+    para.block_width = int((post['width']))
+    para.block_height = int((post['height']))
     stegomsg = libstego.new_charp()
     stegolen = libstego.new_intp()
     libstego.cpt_extract(rgb_data, stegomsg, stegolen, para)
-    return str(libstego.charp_value(stegomsg))
+    q.put(str(libstego.charp_value(stegomsg)))
 
 def f5Embed(q):
     post, filename = q.get()
@@ -232,34 +304,36 @@ def f5Embed(q):
     libstegofile.io_jpeg_write(filename, jpeg_struct)                              
     q.put(filename)
 
-def f5Extract(request):
+def f5Extract(q):
+    post, filename = q.get()
     jpeg_data = libstegofile.jpeg_data_t()
     jpeg_struct = libstegofile.jpeg_internal_data_t()
     para = libstegofile.f5_parameter()
-    para.password = str(request.POST['pw'])
+    para.password = str(post['pw'])
     para.pwlen = len(para.password)
     stegomsg = libstego.new_charp()
     stegolen = libstego.new_intp0()
-    retcode = libstegofile.io_jpeg_read(request.FILES['file'].temporary_file_path(),jpeg_data, jpeg_struct)
-    if retcode == 0 and request.FILES['file'].content_type == "image/jpeg":
+    retcode = libstegofile.io_jpeg_read(filename, jpeg_data, jpeg_struct)
+    if retcode == 0:
         pass
     else:
         return -1
     libstego.f5_extract(jpeg_data, stegomsg, stegolen, para)
-    return str(libstego.charp_value(stegomsg))
+    q.put(str(libstego.charp_value(stegomsg)))
 
-def lsbEmbed(request):
+def lsbEmbed(q):
+    post, filename = q.get()
     rgb_data = libstegofile.rgb_data_t()
     stego_data = libstegofile.rgb_data_t()
     png_struct = libstegofile.png_internal_data_t()
-    retcode = libstegofile.io_png_read(request.FILES['file'].temporary_file_path(),rgb_data, png_struct)
-    if retcode == 0 and request.FILES['file'].content_type == "image/png":
+    retcode = libstegofile.io_png_read(filename, rgb_data, png_struct)
+    if retcode == 0:
         pass
     else:
         return -1
     para = libstegofile.lsb_parameter_t()
-    message = str(request.POST['message'])
-    para.password = str(request.POST['pw'])
+    message = str(post['message'])
+    para.password = str(post['pw'])
     para.pwlen = len(para.password)
     para.select_mode = 2
     para.use_msb = 0
@@ -269,28 +343,22 @@ def lsbEmbed(request):
     libstego.lsb_embed_indirect(libstego.charpp_value(data),
                         libstego.intp_value(datalen), message, len(message), para)
     libstegofile.io_png_integrate(png_struct, rgb_data)
-
-    libstegofile.io_png_write(request.FILES['file'].temporary_file_path(), png_struct)
-    filename = request.FILES['file'].temporary_file_path() 
-    wrapper = FileWrapper(file(filename))
-    response = HttpResponse(wrapper, mimetype='image/png')
-    response['Content-Length'] = os.path.getsize(filename)
-
-    response['Content-Disposition'] = 'attachment; filename=stego.png'
-    return response
+    libstegofile.io_png_write(filename, png_struct)
+    q.put(filename)
     
-def lsbExtract(request):
+def lsbExtract(q):
+    post, filename = q.get()
     rgb_data = libstegofile.rgb_data_t()
     png_struct = libstegofile.png_internal_data_t()
     para = libstegofile.lsb_parameter_t()
-    para.password = str(request.POST['pw'])
+    para.password = str(post['pw'])
     para.pwlen = len(para.password)
     para.select_mode = 2
     para.use_msb = 0
     stegomsg = libstego.new_charp()
     stegolen = libstego.new_intp()
-    retcode = libstegofile.io_png_read(request.FILES['file'].temporary_file_path(),rgb_data, png_struct)
-    if retcode == 0 and request.FILES['file'].content_type == "image/png":
+    retcode = libstegofile.io_png_read(filename, rgb_data, png_struct)
+    if retcode == 0:
         pass
     else:
         return -1
@@ -301,43 +369,39 @@ def lsbExtract(request):
     libstego.lsb_extract_indirect(libstego.charpp_value(bytes),
                         libstego.intp_value(num_bytes), stegomsg, stegolen, para)
     msglen = libstego.intp_value(stegolen)
-    return (str(libstego.charp_value(stegomsg)))[0:msglen]
+    q.put((str(libstego.charp_value(stegomsg)))[0:msglen])
 
-def gifShuffleEmbed(request):
+def gifShuffleEmbed(q):
+    post, filename = q.get()
     palette_data = libstegofile.palette_data_t()
     stego_data = libstegofile.palette_data_t()
     gif_struct = libstegofile.gif_internal_data_t()
-    retcode = libstegofile.io_gif_read(request.FILES['file'].temporary_file_path(),palette_data, gif_struct)
-    if retcode == 0 and request.FILES['file'].content_type == "image/gif":
+    retcode = libstegofile.io_gif_read(filename, palette_data, gif_struct)
+    if retcode == 0:
         pass
     else:
         return -1
     para = libstegofile.gifshuffle_parameter()
-    message = str(request.POST['message'])
-    para.password = str(request.POST['pw'])
+    message = str(post['message'])
+    para.password = str(post['pw'])
     para.pwlen = len(para.password)
     libstego.gifshuffle_embed(palette_data, stego_data, message, len(message), para)
     libstegofile.io_gif_integrate(gif_struct, stego_data)
-    libstegofile.io_gif_write(request.FILES['file'].temporary_file_path(), gif_struct)
-    filename = request.FILES['file'].temporary_file_path() 
-    wrapper = FileWrapper(file(filename))
-    response = HttpResponse(wrapper, mimetype='image/gif')
-    response['Content-Length'] = os.path.getsize(filename)
+    libstegofile.io_gif_write(filename, gif_struct)
+    q.put(filename)
 
-    response['Content-Disposition'] = 'attachment; filename=stego.gif'
-    return response
-
-def gifShuffleExtract(request):
+def gifShuffleExtract(q):
     print "GifExtract..."
+    post, filename = q.get()
     palette_data = libstegofile.palette_data_t()
     gif_struct = libstegofile.gif_internal_data_t()
     para = libstegofile.gifshuffle_parameter()
-    para.password = str(request.POST['pw'])
+    para.password = str(post['pw'])
     para.pwlen = len(para.password)
     stegomsg = libstego.new_charp()
     stegolen = libstego.new_intp()
-    retcode = libstegofile.io_gif_read(request.FILES['file'].temporary_file_path(),palette_data, gif_struct)
-    if retcode == 0 and request.FILES['file'].content_type == "image/gif":
+    retcode = libstegofile.io_gif_read(filename, palette_data, gif_struct)
+    if retcode == 0:
         pass
         print "Valid gif at extracting"
     else:
@@ -345,51 +409,47 @@ def gifShuffleExtract(request):
     print "Extracting..."
     libstego.gifshuffle_extract(palette_data, stegomsg, stegolen, para)
     print "Message: "+ str(libstego.charp_value(stegomsg))
-    return str(libstego.charp_value(stegomsg))
+    q.put(str(libstego.charp_value(stegomsg)))
 
-def bsEmbed(request):
+def bsEmbed(q):
+    post, filename = q.get()
     rgb_data = libstegofile.rgb_data_t()
     stego_data = libstegofile.rgb_data_t()
     png_struct = libstegofile.png_internal_data_t()
-    retcode = libstegofile.io_png_read(request.FILES['file'].temporary_file_path(),rgb_data, png_struct)
-    if retcode == 0 and request.FILES['file'].content_type == "image/png":
+    retcode = libstegofile.io_png_read(filename, rgb_data, png_struct)
+    if retcode == 0:
         pass
     else:
         return -1
     para = libstegofile.battlesteg_parameter()
-    message = str(request.POST['message'])
-    para.password = str(request.POST['pw'])
+    message = str(post['message'])
+    para.password = str(post['pw'])
     para.pwlen = len(para.password)
-    para.startbit = int((request.POST['startbit']))
-    para.move_away = int((request.POST['move_away']))
-    para.range = int((request.POST['range']))
+    para.startbit = int((post['startbit']))
+    para.move_away = int((post['move_away']))
+    para.range = int((post['range']))
     libstego.battlesteg_embed(rgb_data, stego_data, message, len(message), para)
     libstegofile.io_png_integrate(png_struct, stego_data)
-    libstegofile.io_png_write(request.FILES['file'].temporary_file_path(), png_struct)
-    filename = request.FILES['file'].temporary_file_path() 
-    wrapper = FileWrapper(file(filename))
-    response = HttpResponse(wrapper, mimetype='image/png')
-    response['Content-Length'] = os.path.getsize(filename)
+    libstegofile.io_png_write(filename, png_struct)
+    q.put(filename)
 
-    response['Content-Disposition'] = 'attachment; filename=stego.png'
-    return response
-
-def bsExtract(request):
+def bsExtract(q):
+    post, filename = q.get()
     rgb_data = libstegofile.rgb_data_t()
     png_struct = libstegofile.png_internal_data_t()
-    retcode = libstegofile.io_png_read(request.FILES['file'].temporary_file_path(),rgb_data, png_struct)
-    if retcode == 0 and request.FILES['file'].content_type == "image/png":
+    retcode = libstegofile.io_png_read(filename, rgb_data, png_struct)
+    if retcode == 0:
         pass
     else:
         return -1
     para = libstegofile.battlesteg_parameter()
-    para.password = str(request.POST['pw'])
+    para.password = str(post['pw'])
     para.pwlen = len(para.password)
-    para.startbit = int((request.POST['startbit']))
-    para.move_away = int((request.POST['move_away']))
-    para.range = int((request.POST['range']))
+    para.startbit = int((post['startbit']))
+    para.move_away = int((post['move_away']))
+    para.range = int((post['range']))
     stegomsg = libstego.new_charp()
     stegolen = libstego.new_intp()
     libstego.battlesteg_extract(rgb_data, stegomsg, stegolen, para)
     msglen = libstego.intp_value(stegolen)
-    return (str(libstego.charp_value(stegomsg)))[0:msglen]
+    q.put((str(libstego.charp_value(stegomsg)))[0:msglen])
