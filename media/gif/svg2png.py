c036d69a2538f187c5ca3124561e30842c8fca6d
Marko Krause <zeratul2099@googlemail.com>
2009-10-01 04:10:25 +0200

    gifs

--- media/gif/svg2png.py
+++ media/gif/svg2png.py
@@ -2,9 +2,9 @@
 
 import os
 
-name = "md5_i"
+name = "sha_maj"
 
-for i in range(0, 50, 2):
+for i in range(0, 80, 2):
     os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg &"%(name, i, float(i)/4, name))
     os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg"%(name, i+1, float(i+1)/4, name))
 os.chdir("png")
