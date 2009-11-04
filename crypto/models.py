3cb4d839283cb010aa49800cf71f31fe79a4767e
Marko Krause <zeratul2099@googlemail.com>
2009-11-04 14:48:27 +0100

    des and xor

--- crypto/models.py
+++ crypto/models.py
@@ -13,3 +13,12 @@ class AESDecryptForm(forms.Form):
     block_values = ((1,'ECB'),(2,'CBC'),(3,'CFB')) 
     block_mode = forms.ChoiceField(choices=block_values, label='Blockmodus')
 
+
+class SimpleEncryptForm(forms.Form):
+    message = forms.CharField(label='Nachricht', required=True)
+    key = forms.IntegerField(label='Schluessel (grosse Zahl)', required=True)
+
+class SimpleDecryptForm(forms.Form):
+    cypher_text = forms.IntegerField(label='Chiffre', required=True)
+    key = forms.IntegerField(label='Schluessel (grosse Zahl)', required=True)
+
--- crypto/views.py
+++ crypto/views.py
@@ -1,9 +1,9 @@
-from crypt_app.crypto.models import AESEncryptForm, AESDecryptForm
+from crypt_app.crypto.models import AESEncryptForm, AESDecryptForm, SimpleEncryptForm, SimpleDecryptForm
 from crypt_app.base_app.models import Algo
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import Context, loader
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, DES, XOR
 from Crypto.Util import RFC1751 as rfc
 from Crypto.Util import number
 
@@ -30,6 +30,30 @@ def algo(request, algo):
                     output += "AES-verschluesselt:\n"
                     aesObject = AES.new(key, int(request.POST["block_mode"]))
                     cypher = number.bytes_to_long(aesObject.encrypt(plain_text))
+            elif algo == 'des':
+                cypherForm = AESEncryptForm(request.POST)
+                decypherForm = AESDecryptForm()
+                if cypherForm.is_valid():
+                    extend = 8 - (len(plain_text) % 8)
+                    for i in range(extend):
+                        plain_text += "X"
+                    output = "Klartext (erweitert):\n%s\n\n" %(plain_text)
+                    key = number.long_to_bytes(request.POST["key"], 8)[0:8]
+                    output += "Key: %s\n\n"%(request.POST["key"])
+                    output += "DES-verschluesselt:\n"
+                    desObject = DES.new(key, int(request.POST["block_mode"]))
+                    cypher = number.bytes_to_long(desObject.encrypt(plain_text))
+
+            elif algo == 'xor':
+                cypherForm = SimpleEncryptForm(request.POST)
+                decypherForm = SimpleDecryptForm()
+                if cypherForm.is_valid():
+                    output = "Klartext:\n%s\n\n" %(plain_text)
+                    key = number.long_to_bytes(request.POST["key"], 0)
+                    output += "Key: %s\n\n"%(request.POST["key"])
+                    output += "XOR-verschluesselt:\n"
+                    xorObject = XOR.new(key)
+                    cypher = number.bytes_to_long(xorObject.encrypt(plain_text))
             else:
                 output += "Invalid algorithm"
         # decrypt
@@ -42,9 +66,31 @@ def algo(request, algo):
                     output = "Klartext:\n"
                     aesObject = AES.new(key, int(request.POST["block_mode"]))
                     cypher = aesObject.decrypt(number.long_to_bytes(request.POST["cypher_text"]))
+            elif algo == 'des':
+                cypherForm = AESEncryptForm()
+                decypherForm = AESDecryptForm(request.POST)
+                if decypherForm.is_valid():
+                    key = number.long_to_bytes(request.POST["key"], 8)[0:8]
+                    output = "Klartext:\n"
+                    desObject = DES.new(key, int(request.POST["block_mode"]))
+                    cypher = desObject.decrypt(number.long_to_bytes(request.POST["cypher_text"]))                
+            elif algo == 'xor':
+                cypherForm = SimpleEncryptForm()
+                decypherForm = SimpleDecryptForm(request.POST)
+                if decypherForm.is_valid():
+                    key = number.long_to_bytes(request.POST["key"], 0)
+                    output = "Klartext:\n"
+                    xorObject = XOR.new(key)
+                    cypher = xorObject.decrypt(number.long_to_bytes(request.POST["cypher_text"]))                
+            else:
+                output += "Invalid algorithm"
     else:
-        cypherForm = AESEncryptForm()
-        decypherForm = AESDecryptForm()
+        if algo == 'aes' or algo == 'des':
+            cypherForm = AESEncryptForm()
+            decypherForm = AESDecryptForm()
+        elif algo == 'xor':
+            cypherForm = SimpleEncryptForm()
+            decypherForm = SimpleDecryptForm()
     
     return render_to_response("crypto_algo.html", {'title' : algo_object.name,
                                             'algo' : algo,
