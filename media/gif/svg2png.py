149632f0ca0a27999f1349ef02c61b3f191200fc
Marko Krause <zeratul2099@googlemail.com>
2010-03-09 21:12:40 +0100

    more gifs

--- media/gif/svg2png.py
+++ media/gif/svg2png.py
@@ -2,11 +2,11 @@
 
 import os
 
-name = "sha_maj"
+name = "xordecrypt"
 
-for i in range(0, 80, 2):
-    os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg &"%(name, i, float(i)/4, name))
-    os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg"%(name, i+1, float(i+1)/4, name))
+#for i in range(0, 140, 2):
+    #os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg &"%(name, i, float(i)/4, name))
+    #os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg"%(name, i+1, float(i+1)/4, name))
 os.chdir("png")
 os.system("convert -dispose previous -delay 25 -page +0+0 %s*.png %s.gif"%(name, name))
 
