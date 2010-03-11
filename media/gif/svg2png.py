5d90921fdd57f739451971cdc74ca4699a118c2c
Marko Krause <zeratul2099@googlemail.com>
2010-03-12 00:11:46 +0100

    gifs

--- media/gif/svg2png.py
+++ media/gif/svg2png.py
@@ -2,9 +2,9 @@
 
 import os
 
-name = "aeskeys"
+name = "cpt"
 
-for i in range(0, 480, 2):
+for i in range(0, 104, 2):
     os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg &"%(name, i, float(i)/4, name))
     os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg"%(name, i+1, float(i+1)/4, name))
 os.chdir("png")
