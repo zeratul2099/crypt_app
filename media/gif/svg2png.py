6b6559518feef5d8d8ec2f128a6a8b99c3000b95
Marko Krause <zeratul2099@googlemail.com>
2010-03-12 04:02:45 +0100

    gifs

--- media/gif/svg2png.py
+++ media/gif/svg2png.py
@@ -2,9 +2,9 @@
 
 import os
 
-name = "cpt"
+name = "gifextract"
 
-for i in range(0, 104, 2):
+for i in range(0, 280, 2):
     os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg &"%(name, i, float(i)/4, name))
     os.system("rasterizer -d png/%s%3.3d.png -snapshotTime %f %s.svg"%(name, i+1, float(i+1)/4, name))
 os.chdir("png")
