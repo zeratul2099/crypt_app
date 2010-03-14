31ff5146441e6cb95cdebceec5481daa82fa4a07
Marko Krause <zeratul2099@googlemail.com>
2010-03-14 19:29:59 +0100

    added license information

--- hash/views.py
+++ hash/views.py
@@ -1,4 +1,20 @@
 # encoding: utf-8
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 3 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with libstego.  If not, see <http://www.gnu.org/licenses/>.
+#
+#       Copyright 2009 2010 by Marko Krause <zeratul2099@googlemail.com>
+
 from crypt_app.hash.models import HashForm, Sha2Form
 from crypt_app.base_app.models import Algo, InfoPage, ManPage
 from django.http import HttpResponse, Http404, HttpResponseRedirect
