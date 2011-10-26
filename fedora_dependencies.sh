60af2bfff4bf9082a9c6ee44f138e30edc2de3a5
Marko Krause <zeratul2099@googlemail.com>
2011-10-26 22:11:40 +0200

    added fedora dependencies install script

---
+++ fedora_dependencies.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yum install git gcc gcc-c++ python-virtualenv swig cmake httpd mod_wsgi libpng-devel libjpeg-turbo-devel giflib-devel libxml2-devel gmp-devel libsndfile-devel gsl-devel patch openssl-devel
