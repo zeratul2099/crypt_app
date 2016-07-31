83b8098f258e729234958ce96fb3200b00781091
Marko <zeratul@posteo.de>
2016-07-31 12:37:47 +0200

    work on documentation and removed uneccassary files

--- fedora_dependencies.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-yum install git gcc gcc-c++ python-virtualenv swig cmake httpd mod_wsgi libpng-devel libjpeg-turbo-devel giflib-devel libxml2-devel gmp-devel libsndfile-devel gsl-devel patch openssl-devel
--- kryptos_install.sh
+++ kryptos_install.sh
@@ -25,34 +25,32 @@ pip install --upgrade pip
 
 pip install django==1.9.8
 pip install pycrypto==2.6.1
-pip download m2crypto==0.25.1
-#cp remove-sslv2.patch venv/build/m2crypto
-#cd venv/build/m2crypto
-#patch -p0 < remove-sslv2.patch
-#if [ $1 == 'fedora' ]
-#then
-#echo 'building fedora version'
-#bash fedora_setup.sh build
-#bash fedora_setup.sh install
-#else
-#python setup.py build
-#python setup.py install
-#fi
-#cd ../../
-git clone https://github.com/zeratul2099/libstego.git
-cd libstego
-cmake -DPYTHON_LIBRARY=$cwd/venv/include/python2.7 -DCMAKE_CXX_COMPILER=gcc .
-make
-cp swig_bindings/_libstego* ../../stego/
-cp swig_bindings/libstego*.py ../../stego/
-cp src/libstego/libstego.so ../../
-cp src/libstegofile/libstegofile.so ../../
 
-echo 'build finished, please add:'
-echo
-echo 'Alias /content/ ${cwd}/media/'
-echo 'Alias /media/ ${cwd}/venv/lib/python2.7/site-packages/django/contrib/admin/media/'
-echo 'WSGIScriptAlias / ${cwd}/kryptos.wsgi'
-echo 'WSGIRestrictStdin Off'
-echo
-echo 'to your httpd.conf and make sure that ${cwd} and ${cwd}/db are readable/writable by the webserver'
+# m2crypto from git python3 branch
+cd venv
+git clone https://gitlab.com/m2crypto/m2crypto.git
+cd m2crypto
+git checkout python3
+python setup.py build
+python setup.py install
+# setup.py seem not to copy the libs correctly to site-packages, do it manually
+cd build
+cd lib*
+cp -r M2Crypto ../../../lib/python3*/site-packages/
+#git clone https://github.com/zeratul2099/libstego.git
+#cd libstego
+#cmake -DPYTHON_LIBRARY=$cwd/venv/include/python2.7 -DCMAKE_CXX_COMPILER=gcc .
+#make
+#cp swig_bindings/_libstego* ../../stego/
+#cp swig_bindings/libstego*.py ../../stego/
+#cp src/libstego/libstego.so ../../
+#cp src/libstegofile/libstegofile.so ../../
+
+#echo 'build finished, please add:'
+#echo
+#echo 'Alias /content/ ${cwd}/media/'
+#echo 'Alias /media/ ${cwd}/venv/lib/python2.7/site-packages/django/contrib/admin/media/'
+#echo 'WSGIScriptAlias / ${cwd}/kryptos.wsgi'
+#echo 'WSGIRestrictStdin Off'
+#echo
+#echo 'to your httpd.conf and make sure that ${cwd} and ${cwd}/db are readable/writable by the webserver'
